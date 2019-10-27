7fdcbf981eb6416f3224fc1f5f21f241c841d28c
wzpdreamer <3176869767@qq.com>
2019-10-27 20:39:15 +0800

    SWT.GUI

---
+++ src/MainWindow.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+import java.awt.*;
+
+
+public class MainWindow {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("MainWindow");
+//        frame.setContentPane(new MainForm().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(600,500);
+        frame.setPreferredSize(new Dimension(600,500));
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
+
